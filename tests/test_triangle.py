d1f0d0d3b8ad3b517c27dd72a5871742a91b6233
hsiaohsuan <hsiaohsuan1l1l@gmail.com>
2017-04-23 17:13:27 +0800

    Pass statement coverage test for Triangle

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -4,3 +4,9 @@ from TestCov import triangle
 class TestStatementCoverage:
     def test_normal(self):
         assert triangle.Triangle(100, 100, 100) == "Equilateral"
+        assert triangle.Triangle(60, 60, 100) == "Isosceles"
+        assert triangle.Triangle(30, 40, 50) == "Scalene"
+
+    def test_robust(self):
+        assert triangle.Triangle(0, 100, 100) == "NotATriangle"
+        assert triangle.Triangle(50, 50, 100) == "NotATriangle"
