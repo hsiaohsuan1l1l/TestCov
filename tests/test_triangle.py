e47d74705abc1aa516a2963fb8ca47286b5492eb
hsiaohsuan <hsiaohsuan1l1l@gmail.com>
2017-04-23 18:00:06 +0800

    Pass MCDC for Triangle

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -10,3 +10,59 @@ class TestStatementCoverage:
     def test_robust(self):
         assert triangle.Triangle(0, 100, 100) == "NotATriangle"
         assert triangle.Triangle(50, 50, 100) == "NotATriangle"
+
+
+class TestMCDC:
+    def test_statement(self):
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+        assert triangle.Triangle(60, 60, 100) == "Isosceles"
+        assert triangle.Triangle(30, 40, 50) == "Scalene"
+        assert triangle.Triangle(0, 100, 100) == "NotATriangle"
+        assert triangle.Triangle(50, 50, 100) == "NotATriangle"
+
+    def test_boolean(self):
+        assert triangle.Triangle(0, 100, 100) == "NotATriangle"
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+
+        assert triangle.Triangle(50, 50, 100) == "NotATriangle"
+        assert triangle.Triangle(60, 60, 100) == "Isosceles"
+
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+        assert triangle.Triangle(60, 60, 100) == "Isosceles"
+
+        assert triangle.Triangle(60, 60, 100) == "Isosceles"
+        assert triangle.Triangle(30, 40, 50) == "Scalene"
+
+    def test_independent(self):
+        # a not in range(1, 201)
+        assert triangle.Triangle(0, 100, 100) == "NotATriangle"
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+        # b not in range(1, 201)
+        assert triangle.Triangle(100, 0, 100) == "NotATriangle"
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+        # c not in range(1, 201)
+        assert triangle.Triangle(100, 100, 0) == "NotATriangle"
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+
+        # a + b <= c
+        assert triangle.Triangle(50, 50, 100) == "NotATriangle"
+        assert triangle.Triangle(60, 50, 100) == "Scalene"
+        # a + c <= b
+        assert triangle.Triangle(50, 100, 50) == "NotATriangle"
+        assert triangle.Triangle(60, 100, 50) == "Scalene"
+        # b + c <= a
+        assert triangle.Triangle(100, 50, 50) == "NotATriangle"
+        assert triangle.Triangle(100, 60, 50) == "Scalene"
+
+        # a == b
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+        assert triangle.Triangle(100, 50, 100) == "Isosceles"
+        assert triangle.Triangle(60, 50, 100) == "Scalene"
+        # a == c
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+        assert triangle.Triangle(100, 100, 50) == "Isosceles"
+        assert triangle.Triangle(60, 100, 50) == "Scalene"
+        # b == c
+        assert triangle.Triangle(100, 100, 100) == "Equilateral"
+        assert triangle.Triangle(100, 50, 100) == "Isosceles"
+        assert triangle.Triangle(100, 50, 60) == "Scalene"
